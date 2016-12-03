99c9a6623b6687d7aa23a97791724ebee0a0d405
doodlesalot <somewhatrounded@gmail.com>
2016-12-03 11:50:38 -0500

    Added users, a bit of tidying up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  # removed :recoverable until mailer is setup
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :rememberable, :trackable, :validatable
 end
